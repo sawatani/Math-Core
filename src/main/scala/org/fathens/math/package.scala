8d5309e224ea022427d3d6c8d8211a374d1e1eae
Kunio Sawatani <devel@fathens.org>
2015-01-22 00:28:39 +0900

    package 内の関数には型をちゃんと指定

--- src/main/scala/org/fathens/math/package.scala
+++ src/main/scala/org/fathens/math/package.scala
@@ -2,29 +2,29 @@ package org.fathens
 
 package object math {
   // Angular
-  implicit def toRadians(a: Angular[_]) = Angular[Radians](a)
-  implicit def toDegrees(a: Angular[_]) = Angular[Degrees](a)
+  implicit def toRadians(a: Angular[_]): Radians = Angular[Radians](a)
+  implicit def toDegrees(a: Angular[_]): Degrees = Angular[Degrees](a)
 
   // Meter series
-  implicit def toMeters(a: Length[_]) = Length[Meters](a)
-  implicit def toKillometers(a: Length[_]) = Length[Killometers](a)
-  implicit def toMillimeters(a: Length[_]) = Length[Millimeters](a)
-  implicit def toNanometers(a: Length[_]) = Length[Nanometers](a)
+  implicit def toMeters(a: Length[_]): Meters = Length[Meters](a)
+  implicit def toKillometers(a: Length[_]): Killometers = Length[Killometers](a)
+  implicit def toMillimeters(a: Length[_]): Millimeters = Length[Millimeters](a)
+  implicit def toNanometers(a: Length[_]): Nanometers = Length[Nanometers](a)
   // Inch
-  implicit def toInch(a: Length[_]) = Length[Inch](a)
+  implicit def toInch(a: Length[_]): Inch = Length[Inch](a)
 
   implicit class UnitDouble(d: Double) {
     def *[U <: NumUnit[U]](u: U): U = u * d
   }
 
   // Trigonometric Functions
-  def sin(θ: Radians) = scala.math.sin(θ.value)
-  def cos(θ: Radians) = scala.math.cos(θ.value)
-  def tan(θ: Radians) = scala.math.tan(θ.value)
-  def asin(v: Double) = Radians(scala.math.asin(v))
-  def acos(v: Double) = Radians(scala.math.acos(v))
-  def atan(v: Double) = Radians(scala.math.atan(v))
-  def atan2(x: Double, y: Double) = Radians(scala.math.atan2(x, y))
+  def sin(θ: Radians): Double = scala.math.sin(θ.value)
+  def cos(θ: Radians): Double = scala.math.cos(θ.value)
+  def tan(θ: Radians): Double = scala.math.tan(θ.value)
+  def asin(v: Double): Radians = Radians(scala.math.asin(v))
+  def acos(v: Double): Radians = Radians(scala.math.acos(v))
+  def atan(v: Double): Radians = Radians(scala.math.atan(v))
+  def atan2(x: Double, y: Double): Radians = Radians(scala.math.atan2(x, y))
 
   // Calculations
   def sqrt[U <: NumUnit[U]](u: U): U = u withValue scala.math.sqrt(u.value)
